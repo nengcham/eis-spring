ec0c4b5f3e3d4bf6fe0695e5e77cefe9a8b80e8f
nengcham <nengcham7@gmail.com>
2022-05-07 23:23:38 +0900

    0507

--- src/main/java/kr/co/eis/repositories/UserRepository.java
+++ src/main/java/kr/co/eis/repositories/UserRepository.java
@@ -18,8 +18,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    String login(User user);
-
     void put(User user);
+    String login(User user);
 
 }
