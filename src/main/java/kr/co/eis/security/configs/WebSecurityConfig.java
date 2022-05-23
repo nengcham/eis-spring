90bd1e28146893db5595fb8b3c126286c1f0e376
nengcham <nengcham7@gmail.com>
2022-05-23 18:25:03 +0900

    config

---
+++ src/main/java/kr/co/eis/security/configs/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package kr.co.eis.security.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * packageName: kr.co.eis.config
+ * fileName   : WebSecurityConfig
+ * author     : ChoiGeonIl
+ * date       : 2022-05-23
+ * desc       :
+ * ================================
+ * DATE        AUTHOR        NOTE
+ * ================================
+ * 2022-05-23    ChoiGeonIl     최초 생성
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(HttpMethod.OPTIONS, "*/**")
+                .antMatchers("/");
+    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers("/users/join").permitAll()
+                .antMatchers("/users/login").permitAll()
+                .anyRequest().authenticated();
+        http.exceptionHandling().accessDeniedPage("/users/login");
+    }
+}
